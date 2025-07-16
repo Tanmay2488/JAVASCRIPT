6ea73fbe6025fdcd943bc0434e360db1bf958f91
Tanmay2488 <136226681+Tanmay2488@users.noreply.github.com>
2025-07-16 09:37:57 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,6 @@
+a="apple";
+console.log("My name is  TH");
+let b=29;
+let c=30;
+d=b+c;
+console.log("The sum of b and c is: " + d);
---
+++ script2.js
@@ -0,0 +1,27 @@
+let a = 20
+let b = 10;
+c="kojja";
+console.log("bhavesh is " + c);
+let sum = a + b;
+console.log("The sum of a and b is: " + sum);
+let d = b - a;
+console.log("The difference of b and a is: " + d);
+//
+let e = a * b;
+console.log("The product of a and b is: " + e);
+//
+let f = a / b;
+console.log("The division of a by b is: " + f);
+//
+var name = "John";
+var age = 25;
+console.log(age);
+//
+const country= "USA";
+console.log(country);
+//
+const user = {
+  name: "John",
+  age:25
+};
+console.log(user.name);
